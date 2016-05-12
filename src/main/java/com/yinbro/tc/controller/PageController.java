7422a45a6973b3aa8df7a900aeb5a5e55be3c08b
yinbro <yinbro@foxmail.com>
2016-05-12 23:04:03 +0800

    1.解决无法进入控制器的问题，原因是没有启用 <mvc:annotation-driven />-----感谢澳奇 2.静态资源文件的问题

--- src/main/java/com/yinbro/tc/controller/PageController.java
+++ src/main/java/com/yinbro/tc/controller/PageController.java
@@ -1,27 +1,24 @@
 package com.yinbro.tc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping()
 public class PageController {
-	
-	
 
-	@RequestMapping("/login")
-	public String loginPage() {
+	@RequestMapping("login")
+	public String loginPage(Model model) {
 		System.out.println("Login Controller");
 		return "login";
 	}
 	
-	
-	@RequestMapping("/capsuleDetail.do")
+	@RequestMapping("/capsuleDetail")
 	public String capsuleDetail() {
-		System.out.println("Controller2");
+		System.out.println("capsuleDetail Controller");
 		return "capsule-detail";
 	}
-	
-	
 
 
 }
